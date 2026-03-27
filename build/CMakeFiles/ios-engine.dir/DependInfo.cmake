b075a931d195dea724bd9d6f36d7824e5a476de9
g 5 <g5@gs-MacBook-Pro.lan>
2026-03-27 19:33:28 +0100

    Rename tutorials: TutorialPPKmanual.html, TutorialMLKEM.html
    Created by G5 (Giuseppe Cinque) in collaboration with AI

---
+++ build/CMakeFiles/ios-engine.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/g5/Claude-Projects/Code copy/cisco-pqc-vpn-wasm/src/bindings.cpp" "CMakeFiles/ios-engine.dir/src/bindings.cpp.o" "gcc" "CMakeFiles/ios-engine.dir/src/bindings.cpp.o.d"
+  "/Users/g5/Claude-Projects/Code copy/cisco-pqc-vpn-wasm/src/commands/config_gen.cpp" "CMakeFiles/ios-engine.dir/src/commands/config_gen.cpp.o" "gcc" "CMakeFiles/ios-engine.dir/src/commands/config_gen.cpp.o.d"
+  "/Users/g5/Claude-Projects/Code copy/cisco-pqc-vpn-wasm/src/commands/ping.cpp" "CMakeFiles/ios-engine.dir/src/commands/ping.cpp.o" "gcc" "CMakeFiles/ios-engine.dir/src/commands/ping.cpp.o.d"
+  "/Users/g5/Claude-Projects/Code copy/cisco-pqc-vpn-wasm/src/commands/show.cpp" "CMakeFiles/ios-engine.dir/src/commands/show.cpp.o" "gcc" "CMakeFiles/ios-engine.dir/src/commands/show.cpp.o.d"
+  "/Users/g5/Claude-Projects/Code copy/cisco-pqc-vpn-wasm/src/core/cli_parser.cpp" "CMakeFiles/ios-engine.dir/src/core/cli_parser.cpp.o" "gcc" "CMakeFiles/ios-engine.dir/src/core/cli_parser.cpp.o.d"
+  "/Users/g5/Claude-Projects/Code copy/cisco-pqc-vpn-wasm/src/core/device.cpp" "CMakeFiles/ios-engine.dir/src/core/device.cpp.o" "gcc" "CMakeFiles/ios-engine.dir/src/core/device.cpp.o.d"
+  "/Users/g5/Claude-Projects/Code copy/cisco-pqc-vpn-wasm/src/core/ip_util.cpp" "CMakeFiles/ios-engine.dir/src/core/ip_util.cpp.o" "gcc" "CMakeFiles/ios-engine.dir/src/core/ip_util.cpp.o.d"
+  "/Users/g5/Claude-Projects/Code copy/cisco-pqc-vpn-wasm/src/engine.cpp" "CMakeFiles/ios-engine.dir/src/engine.cpp.o" "gcc" "CMakeFiles/ios-engine.dir/src/engine.cpp.o.d"
+  "/Users/g5/Claude-Projects/Code copy/cisco-pqc-vpn-wasm/src/modes/mode_crypto.cpp" "CMakeFiles/ios-engine.dir/src/modes/mode_crypto.cpp.o" "gcc" "CMakeFiles/ios-engine.dir/src/modes/mode_crypto.cpp.o.d"
+  "/Users/g5/Claude-Projects/Code copy/cisco-pqc-vpn-wasm/src/modes/mode_global.cpp" "CMakeFiles/ios-engine.dir/src/modes/mode_global.cpp.o" "gcc" "CMakeFiles/ios-engine.dir/src/modes/mode_global.cpp.o.d"
+  "/Users/g5/Claude-Projects/Code copy/cisco-pqc-vpn-wasm/src/modes/mode_iface.cpp" "CMakeFiles/ios-engine.dir/src/modes/mode_iface.cpp.o" "gcc" "CMakeFiles/ios-engine.dir/src/modes/mode_iface.cpp.o.d"
+  "/Users/g5/Claude-Projects/Code copy/cisco-pqc-vpn-wasm/src/modes/mode_line.cpp" "CMakeFiles/ios-engine.dir/src/modes/mode_line.cpp.o" "gcc" "CMakeFiles/ios-engine.dir/src/modes/mode_line.cpp.o.d"
+  "/Users/g5/Claude-Projects/Code copy/cisco-pqc-vpn-wasm/src/modes/mode_priv.cpp" "CMakeFiles/ios-engine.dir/src/modes/mode_priv.cpp.o" "gcc" "CMakeFiles/ios-engine.dir/src/modes/mode_priv.cpp.o.d"
+  "/Users/g5/Claude-Projects/Code copy/cisco-pqc-vpn-wasm/src/modes/mode_router.cpp" "CMakeFiles/ios-engine.dir/src/modes/mode_router.cpp.o" "gcc" "CMakeFiles/ios-engine.dir/src/modes/mode_router.cpp.o.d"
+  "/Users/g5/Claude-Projects/Code copy/cisco-pqc-vpn-wasm/src/modes/mode_user.cpp" "CMakeFiles/ios-engine.dir/src/modes/mode_user.cpp.o" "gcc" "CMakeFiles/ios-engine.dir/src/modes/mode_user.cpp.o.d"
+  "/Users/g5/Claude-Projects/Code copy/cisco-pqc-vpn-wasm/src/network/acl.cpp" "CMakeFiles/ios-engine.dir/src/network/acl.cpp.o" "gcc" "CMakeFiles/ios-engine.dir/src/network/acl.cpp.o.d"
+  "/Users/g5/Claude-Projects/Code copy/cisco-pqc-vpn-wasm/src/network/routing.cpp" "CMakeFiles/ios-engine.dir/src/network/routing.cpp.o" "gcc" "CMakeFiles/ios-engine.dir/src/network/routing.cpp.o.d"
+  "/Users/g5/Claude-Projects/Code copy/cisco-pqc-vpn-wasm/src/network/vpn.cpp" "CMakeFiles/ios-engine.dir/src/network/vpn.cpp.o" "gcc" "CMakeFiles/ios-engine.dir/src/network/vpn.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ios-engine.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ios-engine.
+# This may be replaced when dependencies are built.
